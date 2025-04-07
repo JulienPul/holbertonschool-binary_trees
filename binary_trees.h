58784f6cc3e94592cdda2d9ac1518732ff76f8e8
Pulon Julien <pulonjulien@gmail.com>
2025-04-07 10:24:31 +0200

    binary_trees.h

---
+++ binary_trees.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+struct binary_tree_s {
+    int n;
+	struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+
+#endif MAIN_H
